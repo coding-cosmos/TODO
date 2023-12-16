5bb9d3c86993bb8807285eb2ac1f15c438b8279a
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-16 12:59:32 +0530

    Add Project class

--- src/Project.js
+++ src/Project.js
@@ -1,3 +1,11 @@
 class Project{
-    
+    constructor() {
+        this.todoList = []
+    }
+    addTodo(todo) {
+        this.todoList.push(todo);
+    }
+    getProject() {
+        return this.todoList;
+    }
 }
