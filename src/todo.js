2a7347f70ea68d50c93d2380067a6ec42b65a277
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-16 12:54:46 +0530

    Add Todo class

---
+++ src/todo.js
@@ -0,0 +1,33 @@
+class Todo {
+  constructor(title, dueDate, description, priority, status) {
+    this.title = title;
+    this.dueDate = dueDate;
+    this.description = description;
+    this.priority = priority;
+    this.status = status;
+  }
+  setDescription(description) {
+    this.description = description;
+  }
+  setPriority(priority) {
+    this.priority = priority;
+  }
+  setStatus(status) {
+    this.status = status;
+  }
+  setDueDate(newDate) {
+    this.dueDate = newDate;
+  }
+  setTitle(title) {
+    this.title = title;
+  }
+  getTodo() {
+    return {
+      title: this.title,
+      dueDate: this.dueDate,
+      description: this.description,
+      priority: this.priority,
+      status: this.status,
+    };
+  }
+}
