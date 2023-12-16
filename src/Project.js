8fd84f6cf3fa906c5ddbb396e28e77fe21ef163a
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-16 13:06:09 +0530

    Add name

--- src/Project.js
+++ src/Project.js
@@ -1,11 +1,15 @@
 class Project{
-    constructor() {
-        this.todoList = []
+    constructor(name) {
+        this.todoList = [];
+        this.name = name;
     }
     addTodo(todo) {
         this.todoList.push(todo);
     }
     getProject() {
-        return this.todoList;
+        return ({
+            todos: this.todoList,
+            name: this.name
+        });
     }
 }
