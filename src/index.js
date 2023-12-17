b2b320471a8552d7d18b3ee57505725517a1dc79
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-17 22:11:46 +0530

    Add main content to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import Sidebar from "./Sidebar";
 import './styles/styles.css';
 import ProjectViewListController from "./ProjectViewListController.js";
+import Main from "./main.js";   
 
 
 function render() {
     const container = document.querySelector('#container');
     container.appendChild(Sidebar());
+     container.appendChild(Main());
     ProjectViewListController.renderData();
+   
 }
 render();
