80eb96374a3b8695466a90fd3e205721f31ac81a
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-17 22:11:23 +0530

    Add main.js

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './styles/main-styles.css';
+
+function Main() {
+    const main = document.createElement('div');
+    main.className = 'main';
+
+    return main;
+}
+
+export default Main;
