c59b71f8902763b2d1288309c3b34c35a9c0dfb6
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-17 20:59:05 +0530

    Add comments

--- src/ProjectViewListController.js
+++ src/ProjectViewListController.js
@@ -2,33 +2,50 @@ import ProjectView from "./ProjectView";
 import Data from "./data";
 
 class ProjectViewListController {
+  // stores the id of previous project
   static prvID;
+
+  // add editable view to projectList
   static addProjectView() {
     const projectList = document.querySelector(".project-list");
-    const newProjectView = new ProjectView(true,"",++ProjectViewListController.prvID);
+    const newProjectView = new ProjectView(
+      true,
+      "",
+      ++ProjectViewListController.prvID
+    );
     projectList.appendChild(newProjectView);
   }
 
   static removeProject(project) {
-    project.remove();
-    const id = project.querySelector('input').getAttribute('data-projectID');
-  
+    // get id of project to remove
+    const id = project.querySelector("input").getAttribute("data-projectID");
+
+    // remove project from localStorage
     Data.removeProject(id);
+
+    // update UI
     ProjectViewListController.renderData();
   }
 
   static addProject(nameInput) {
-    Data.addProject(nameInput.value, nameInput.getAttribute('data-projectID'));
-    
+    // Add project to localStorage
+    Data.addProject(nameInput.value, nameInput.getAttribute("data-projectID"));
 
+    // Updates UI
+    ProjectViewListController.renderData();
   }
 
+  // Updates UI
   static renderData() {
     const projectViewList = document.querySelector(".project-list");
-    projectViewList.innerHTML = "";
-    const projectList = Data.getProjectList();  
-    console.log(projectList);
 
+    // Clears the list
+    projectViewList.innerHTML = "";
+
+    // Get list from localStorage
+    const projectList = Data.getProjectList();
+
+    // Update UI
     projectList.forEach((project) => {
       ProjectViewListController.prvID = project.projectID;
       const projectView = new ProjectView(
