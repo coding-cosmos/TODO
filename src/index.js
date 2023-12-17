c00b9e7dc4d8ce6c401759719791e1b47738e20d
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-17 20:41:58 +0530

    Add ProjectViewListController

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Sidebar from "./Sidebar";
 import './styles/styles.css';
+import ProjectViewListController from "./ProjectViewListController.js";
+
 
 function render() {
     const container = document.querySelector('#container');
     container.appendChild(Sidebar());
-
+    ProjectViewListController.renderData();
 }
 render();
