1ec2ac6ffb7089c1c9a466eff000e4f0ae5ef711
coding-cosmos <115341188+coding-cosmos@users.noreply.github.com>
2023-12-19 19:50:08 +0530

    Add call to render main

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import Sidebar from "./Sidebar";
 import './styles/styles.css';
 import ProjectViewListController from "./ProjectViewListController.js";
 import Main from "./main.js";   
+import MainController from "./MainController.js";
 
 
 function render() {
     const container = document.querySelector('#container');
     container.appendChild(Sidebar());
-     container.appendChild(Main());
     ProjectViewListController.renderProjectList();
-   
+    container.appendChild(Main());
+    MainController.renderMain();
 }
 render();
